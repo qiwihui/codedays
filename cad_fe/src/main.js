ef1575a3cfc227d9fb89f28321c5bc673eb16bc4
qiwihui <qwh005007@gmail.com>
2020-03-05 10:01:04 +0800

    feat: add google analytics

--- cad_fe/src/main.js
+++ cad_fe/src/main.js
@@ -4,8 +4,13 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import Vuex from 'vuex'
+import VueAnalytics from 'vue-analytics'
 
 Vue.use(Vuex)
+Vue.use(VueAnalytics, {
+  id: 'UA-159858108-1',
+  router
+})
 
 // Make Axios play nice with Django CSRF
 axios.defaults.xsrfCookieName = 'csrftoken'
