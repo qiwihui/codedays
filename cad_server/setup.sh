87cbbd0d180dfa0c4bf1f9feb1a330375e4c9f93
qiwihui <qwh005007@gmail.com>
2020-03-05 16:34:52 +0800

    fix: fix setup

--- cad_server/setup.sh
+++ cad_server/setup.sh
@@ -3,4 +3,4 @@ apt update -y
 apt install -y supervisor
 pip install -r requirements.txt
 mkdir -p /var/log/celery/ /var/log/codeaday/
-cp cad_server_celery* /etc/supervisor/conf.d/
+cp cad_server_*.conf /etc/supervisor/conf.d/
