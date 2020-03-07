8346dbee747ff95d5ee2ada8dd5054984c733541
qiwihui <qwh005007@gmail.com>
2020-03-07 15:56:13 +0800

    fix: fix working directory

---
+++ cad_server/cad_server/local_settings_prod.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+SECRET_KEY = '{SECRET_KEY}'
+
+DEBUG = False
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'NAME': 'codeaday',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'codeaday',
+        'PASSWORD': '{DATABASE_PASSWORD}',
+        'HOST': 'codeaday-db',
+        'PORT': '3306',
+    }
+}
+
+MAILGUN_API_KEY = "{MAILGUN_API_KEY}"
+
+ENCRYPT_KEY = b'{ENCRYPT_KEY}'
+
+# redis
+BROKER_URL = 'redis://codeaday-redis:6379/6'
+# store task results in redis
+CELERY_RESULT_BACKEND = 'redis://codeaday-redis:6379/6'
