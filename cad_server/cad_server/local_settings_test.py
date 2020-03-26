4e6155f691f7d9c8882b323856736cfdc603bf5e
qiwihui <qwh005007@gmail.com>
2020-03-26 11:17:45 +0800

    fix: fix local settings

---
+++ cad_server/cad_server/local_settings_test.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = '80l6h^-p@y1ghasdfc-n!ees4cg_lp95$)zgp9+gp%qe@!-n'
+
+DEBUG = True
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'NAME': 'test',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'mysql',
+        'PORT': '3306',
+    }
+}
+
+ANYMAIL = {
+    "MAILGUN_API_KEY": 'fake-mailgun-api-key',
+    "MAILGUN_SENDER_DOMAIN": "mail.codedays.app"
+}
+
+
+ENCRYPT_KEY = b'fake-key'
+
+# redis
+BROKER_URL = 'redis://codedays-redis:6379/6'
+# store task results in redis
+CELERY_RESULT_BACKEND = 'redis://codedays-redis:6379/6'
